42c55a5fca421f734d1f6c3744a03934dd7ed830
Markus Rickert <rickert@fortiss.org>
2019-11-20 00:16:24 +0100

    Update to pass audit

--- Formula/pqp.rb
+++ Formula/pqp.rb
@@ -1,16 +1,16 @@
 class Pqp < Formula
-  desc "A library for performing proximity queries on a pair of geometric models composed of triangles"
+  desc "Library for performing proximity queries on a pair of triangle meshes"
   homepage "https://github.com/GammaUNC/PQP"
   url "https://github.com/GammaUNC/PQP/archive/e583e835532a94511bf217e999a96952db8fea52.tar.gz"
-  sha256 "233ce59c4358caf080c3d2295c0beb2631e0b877a8c3209f401d28c40ea181f6"
   version "1.3"
+  sha256 "233ce59c4358caf080c3d2295c0beb2631e0b877a8c3209f401d28c40ea181f6"
   head "https://github.com/GammaUNC/PQP/archive/master.tar.gz"
 
-  patch do
-    url "https://github.com/GammaUNC/PQP/pull/1.diff"
-  end
-
   depends_on "cmake" => :build
+
+  patch do
+    url "https://github.com/GammaUNC/PQP/pull/1.diff?full_index=1"
+  end
 
   def install
     mkdir "build" do
--- Formula/rl.rb
+++ Formula/rl.rb
@@ -1,33 +1,32 @@
 class Rl < Formula
-  desc "A self-contained C++ library for robot kinematics, motion planning and control"
+  desc "Self-contained C++ library for robot kinematics, motion planning and control"
   homepage "https://www.roboticslibrary.org/"
-  url "https://github.com/roboticslibrary/rl/archive/0.7.0.tar.gz"
-  sha256 "0a8b4c45307dd607bd1c8f56d0f96d40855ad0a697a2642bf3b307c7eb1b3de5"
-  version "0.7.0"
   head "https://github.com/roboticslibrary/rl/archive/master.tar.gz"
 
-  depends_on "boost"
-  depends_on "bullet" => :recommended
+  stable do
+    url "https://github.com/roboticslibrary/rl/archive/0.7.0.tar.gz"
+    sha256 "0a8b4c45307dd607bd1c8f56d0f96d40855ad0a697a2642bf3b307c7eb1b3de5"
+    patch do
+      url "https://github.com/roboticslibrary/rl/commit/2398cea996dce6c54330d6562fa1079daa1a9d4a.patch?full_index=1"
+      sha256 "848679985d67ed7ce5f26e92c1bbcc4013d876291a8b1ea4dfd8f0e6607b6688"
+    end
+    patch do
+      url "https://github.com/roboticslibrary/rl/commit/8bfd262f7eaf23e298396ae0d3f7c360f20d2db1.patch?full_index=1"
+      sha256 "5e736f8b7ffbc53c7973c6360a07bb57e89f33a7050eecf63c0acd28c8290ac8"
+    end
+  end
+
   depends_on "cmake" => :build
+  depends_on "boost"
   depends_on "coin"
   depends_on "eigen"
-  depends_on "nlopt" => [:recommended, "without-numpy"]
+  depends_on "bullet" => :recommended
+  depends_on "nlopt" => :recommended
   depends_on "ode" => :recommended
   depends_on "pqp" => :recommended
   depends_on "qt" => :recommended
   depends_on "solid" => :recommended
   depends_on "soqt" => :recommended
-
-  stable do
-    patch do
-      url "https://github.com/roboticslibrary/rl/commit/2398cea996dce6c54330d6562fa1079daa1a9d4a.patch"
-      sha256 "13ffdf2d9ad3f031752d640babc4040d38876784c23c48ba7c50c6c1a0175e29"
-    end
-    patch do
-      url "https://github.com/roboticslibrary/rl/commit/8bfd262f7eaf23e298396ae0d3f7c360f20d2db1.patch"
-      sha256 "4bc0a110ad883af7e90a28717dabd0a473b27dd648fc1d264e16cef57375d625"
-    end
-  end
 
   def install
     mkdir "build" do
