0d20a585b11f319fa41386d9b3d8e64daedcaaba
Markus Rickert <rickert@fortiss.org>
2020-04-20 01:10:27 +0200

    Update simage

--- Formula/simage.rb
+++ Formula/simage.rb
@@ -1,48 +1,34 @@
 class Simage < Formula
   desc "Image format loaders and front-ends to common import libraries"
-  homepage "https://bitbucket.org/Coin3D/simage"
-  version "1.7.1a-1"
+  homepage "https://github.com/coin3d/simage"
+  version "1.8.0-1"
 
   stable do
-    url "https://bitbucket.org/Coin3D/simage/get/afd8852f40c8.tar.gz"
-    sha256 "bd6a2bcdfa036585d830ab5ea29c42991122dd89ea8d300b580168ffecb14f1b"
-    patch :DATA
-    resource "cpack.d" do
-      url "https://bitbucket.org/Coin3D/cpack.d/get/cf223b81fd77.tar.gz"
-      sha256 "dab707c42138b42c0e1cc9bbae6a087004ab4717ec76a69b5b8dc20ac5b412ba"
-    end
+    url "https://github.com/coin3d/simage.git", :revision => "53ab9d3124e91a46b743e2eef67b1f5abb6e2dc5"
   end
 
   bottle do
     root_url "https://dl.bintray.com/roboticslibrary/bottles-rl"
     cellar :any
-    sha256 "9fbb57e620d3e25c8e468b4aac6b613b3692248fcd799c46064e59fec24dd15c" => :catalina
-    sha256 "19b2c609706216ef928ebe269acc297899fdf855c6c5566198c9977cc1e28a47" => :mojave
+    sha256 "7f5f3f7255a4b85f230b739313cd0c7b2a1d60f770bfd01ce3eafc1625da99e7" => :catalina
+    sha256 "42553fdf018bf4cae2aa069da6e8d922cbf6ef1ff606d7f0769514feb16bb969" => :mojave
   end
 
   head do
-    url "https://bitbucket.org/Coin3D/simage/get/default.tar.gz"
-    resource "cpack.d" do
-      url "https://bitbucket.org/Coin3D/cpack.d/get/default.tar.gz"
-    end
+    url "https://github.com/coin3d/simage.git"
   end
 
   depends_on "cmake" => :build
   depends_on "giflib" => :optional
-  depends_on "jasper" => :optional
   depends_on "jpeg" => :optional
   depends_on "libpng" => :optional
   depends_on "libsndfile" => :optional
   depends_on "libtiff" => :optional
   depends_on "libvorbis" => :optional
-  depends_on "qt" => :optional
 
   def install
-    resource("cpack.d").stage do
-      cp_r Dir.pwd, buildpath/"cpack.d"
-    end
     mkdir "build" do
-      system "cmake", "..", *std_cmake_args
+      system "cmake", "..", *std_cmake_args, "-DSIMAGE_BUILD_EXAMPLES=OFF", "-DSIMAGE_BUILD_TESTS=OFF"
       system "make", "install"
     end
   end
@@ -52,33 +38,54 @@ class Simage < Formula
       cmake_minimum_required(VERSION 3.0)
       project(testSimage)
       find_package(simage)
-      add_executable(testSimage testSimage.cpp)
+      add_executable(testSimage testSimage.c)
       target_link_libraries(testSimage simage::simage)
     EOS
-    (testpath/"testSimage.cpp").write <<~EOS
+    (testpath/"testSimage.c").write <<~EOS
       #include <simage.h>
-      int main() {
-        int major;
-        int minor;
-        int micro;
-        simage_version(&major, &minor, &micro);
+      #include <stdio.h>
+      #include <stdlib.h>
+      int main(int argc, char** argv) {
+        if (argc < 2) {
+          printf("Usage: testSimage FILENAME WIDTH HEIGHT NUMCOMPONENTS\\n");
+          return 1;
+        }
+        if (!simage_check_supported(argv[1])) {
+          printf("File format not supported\\n");
+          return 1;
+        }
+        int width;
+        int height;
+        int numcomponents;
+        unsigned char* imagedata = simage_read_image(argv[1], &width, &height, &numcomponents);
+        if (NULL == imagedata) {
+          printf("Could not load file: %s\\n", simage_get_last_error());
+          return 1;
+        }
+        simage_free_image(imagedata);
+        printf("width: %i\\nheight: %i\\nnumcomponents: %i\\n", width, height, numcomponents);
+        if (width != atoi(argv[2])) {
+          printf("width mismatch\\n");
+          return 1;
+        }
+        if (height != atoi(argv[3])) {
+          printf("height mismatch\\n");
+          return 1;
+        }
+        if (numcomponents != atoi(argv[4])) {
+          printf("numcomponents mismatch\\n");
+          return 1;
+        }
         return 0;
       }
     EOS
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make"
-      system "./testSimage"
+      system "./testSimage", test_fixtures("test.gif"), "1", "1", "4"
+      system "./testSimage", test_fixtures("test.jpg"), "1", "1", "4"
+      system "./testSimage", test_fixtures("test.png"), "8", "8", "1"
+      system "./testSimage", test_fixtures("test.tiff"), "1", "1", "4"
     end
   end
 end
-
-__END__
-diff --git a/packaging/macosx/Welcome.rtf b/packaging/macosx/Welcome.rtf
-new file mode 100644
-index 0000000..08778d7
---- /dev/null
-+++ b/packaging/macosx/Welcome.rtf
-@@ -0,0 +1 @@
-+{\rtf1}
-\ No newline at end of file
